30af0ff341361fa6e13ced4c142d0e6d02883a16
Paul O'Fallon <pofallon@users.noreply.github.com>
2025-04-04 22:13:24 -0400

    Separate lifecycle methods

---
+++ server/src/controllers/apps/index.ts
@@ -0,0 +1,10 @@
+// This file acts as an aggregator for the app controller functions.
+// It currently re-exports everything from apps.ts to facilitate incremental migration.
+
+const appsController = require("./apps");
+const lifecycle = require("./lifecycle");
+
+module.exports = {
+  ...appsController,
+  ...lifecycle,
+};
