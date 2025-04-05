41e765e95c04753a4f9b8bd105f19d8d9047d91d
Paul O'Fallon <pofallon@users.noreply.github.com>
2025-04-05 08:53:06 -0400

    Separate system and encrypted config methods

--- server/src/controllers/config/index.ts
+++ server/src/controllers/config/index.ts
@@ -1,12 +1,9 @@
 // This file acts as an aggregator for the config controller functions.
-
-const configController = require("./config");
 const system = require("./system");
 const app = require("./app");
 const encrypted = require("./encrypted");
 
 module.exports = {
-  ...configController,
   ...system,
   ...app,
   ...encrypted,
